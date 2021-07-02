37a550774180ae822707ee5f2a07e6e6565359f6
Vinicius Nogueira <viniciusepn@gmail.com>
2021-07-01 21:32:29 -0300

    interface

---
+++ 03_RestWithASPNET5Udemy_Person/RestWithASPNET5Udemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace RestWithASPNET5Udemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
